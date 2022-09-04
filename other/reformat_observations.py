4f4cfc466f1ca249f2dc30ef8c5c916811cbb13b
Eduardo Valero Cano <eduardo.valerocano@kaust.edu.sa>
2022-09-04 18:47:45 +0300

    minor modifications

--- other/reformat_observations.py
+++ other/reformat_observations.py
@@ -7,24 +7,20 @@ from obspy import read
 
 # PARAMETERS
 # ==========
-data_path = '/data/valeroe/cvc_correlations/events/observations'
+data_path = '/data/valeroe/sub_cvc_correlations/events/observations'
 data_format = 'sac'
-
-output_format = 'semd'  # mseed, sac, semd
-
-new_nt = 400
-new_dt = 1.0
-shift = -2.0  # specfem shift
-
-rename_data = True
 component = 'HXZ'
+correlation = True
+
+new_dt = None
+new_duration = 80.0
+shift = 0.0  # specfem shift
+output_format = 'semd'  # mseed, sac, semd
+rename_data = True
+
 
 # DONT EDIT BELOW THIS LINE
 # =========================
-new_duration = (new_nt - 1) * new_dt
-new_fs = 1. / new_dt
-nyquist_fq = new_fs / 2.0
-
 events = os.listdir(data_path)
 events = [e for e in events
           if os.path.isdir(os.path.join(data_path, e))]
@@ -53,32 +49,43 @@ for event in events:
         tr.stats.network = rec_net
         tr.stats.station = rec_sta
 
-        if tr.stats.sampling_rate < nyquist_fq:
-            print('New sampling rate is greater than current sampling rate')
-            sys.exit()
+        # resample data
+        if new_dt:
+            new_fs = 1. / new_dt
+            nyquist_fq = new_fs / 2.0
 
-        # antialiasing filter
-        if tr.stats.sampling_rate > nyquist_fq:
-            tr.taper(0.05)
-            tr.filter('lowpass', freq=nyquist_fq, zerophase=True, corners=8)
+            if tr.stats.sampling_rate < nyquist_fq:
+                print('New sampling rate is larger than current sampling rate')
+                sys.exit()
 
-        # downsample
-        tr.interpolate(method='lanczos',
-                       sampling_rate=new_fs,
-                       a=1.0)
+            if tr.stats.sampling_rate > nyquist_fq:
+                tr.taper(0.05)
+                tr.filter('lowpass', freq=nyquist_fq, zerophase=True,
+                          corners=8)
 
-        tr.stats.sampling_rate = new_fs
+            tr.interpolate(method='lanczos',
+                           sampling_rate=new_fs,
+                           a=1.0)
 
-        # if output format is semd, zero-pad trace start to account for
+            tr.stats.sampling_rate = new_fs
+            tr.stats.delta = new_dt
+
+        # cut data
+        if new_duration:
+            if correlation:
+                maxlag = ((tr.stats.npts - 1) / 2) * tr.stats.delta
+                tr.trim(tr.stats.starttime + maxlag - new_duration,
+                        tr.stats.starttime + maxlag + new_duration)
+            else:
+                tr.trim(tr.stats.starttime, tr.stats.starttime + new_duration)
+
+        # if output format is semd, zero-pad data start to account for
         # source function shift
         if output_format == 'semd':
             tr.trim(tr.stats.starttime + shift, tr.stats.endtime,
                     pad=True, fill_value=0.)
 
-        # cut trace to new duration
-        tr.trim(tr.stats.starttime, tr.stats.starttime + new_duration)
-
-        # save trace
+        # save data
         if rename_data:
             out_name = '{}.{}.{}'.format(rec, component, output_format)
         else:
@@ -88,6 +95,10 @@ for event in events:
 
         if output_format == 'semd':
             times = tr.times()
+
+            if correlation:
+                times -= new_duration
+
             amp = tr.data
 
             out = np.zeros((times.shape[0], 2))
