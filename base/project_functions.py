b574d9972b1e7047cf56e2668b7b01b6fa3eca10
Eduardo Valero Cano <eduardo.valerocano@kaust.edu.sa>
2023-09-07 13:55:21 +0300

    starting: modifiying workflow to process multiple components

--- base/project_functions.py
+++ base/project_functions.py
@@ -2,47 +2,143 @@ import datetime
 import itertools
 import numpy as np
 import os
-import pandas as pd
 import pickle
+import warnings
 from obspy import read_inventory, UTCDateTime
 from obspy.geodetics import gps2dist_azimuth
 
 
-def list_waveforms_perday(waveforms_paths, data_span):
-    waveforms_paths_perday = {}
+def scan_stations(metadata_path, cmpts, ignore_net=None, ignore_sta=None):
+    """
+    Each metadata file must contain information about only one station
+    """
+    for cmp in cmpts:
+        assert(cmp in ["N","E","Z"]),("Incorrect component")
 
-    for day in data_span:
-        # determine file-name prefix for the day
-        day1 = day.replace("-", "")
-        day2 = UTCDateTime(day) + 86400
-        day2 = str(day2.date)
-        day2 = day2.replace("-", "")
-        day_prefix = "%sT000000Z.%sT000000Z" % (day1, day2)
+    acceptable_channels = ["BHN","BHE","BHZ","HHN","HHE","HHZ"]
 
-        waveforms = []
+    metadata_files = os.listdir(metadata_path)
+    stations = {}
 
-        for i, tmp in enumerate(waveforms_paths):
-            net, sta, fname = tmp.split('/')
+    for file_ in metadata_files:
+        inv = read_inventory(os.path.join(metadata_path, file_))
+        inv_net, inv_sta, inv_fmt = file_.split(".")
 
-            if day_prefix in fname:
-                waveforms.append(waveforms_paths[i])
+        # skip unwanted inventories
+        if ignore_net and inv_net in ignore_net:
+            continue
 
-        waveforms_paths_perday[day] = waveforms
+        if ignore_sta and f"{inv_net}.{inv_sta}" in ignore_sta:
+            continue
 
-    # verify all waveforms are listed
-    counter = 0
+        # check that there is only one station in the inventory
+        unique_networks = list(set(inv.get_contents()["networks"]))
+        unique_stations = list(set(inv.get_contents()["stations"]))
 
-    for key in waveforms_paths_perday.keys():
-        counter += len(waveforms_paths_perday[key])
+        if len(unique_networks) > 1 or len(unique_stations) > 1:
+            warnings.warn(f"Skipping {file_}: there is metadata about "
+                          f"more than one station.")
+            continue
 
-    if counter == len(waveforms_paths):
-        return waveforms_paths_perday
-    else:
-        print('Something went wrong')
-        return
+        # check that the metadata filename matches the station it contains
+        if f"{inv_net}.{inv_sta}" != unique_stations[0].split(" ")[0]:
+            warnings.warn(f"Skipping {file_}: the file name does not match "
+                          f"the name of the station in the metadata.")
+            continue
+
+        # at this point we are sure the inventory contains information only
+        # about one station, however, sometimes the station configuration
+        # changed during its deployement, resulting in multiple network,
+        # station, and channel objects: loop through them
+        lon = []
+        lat = []
+        elv = []
+        cha = []
+
+        for net_obj in inv:
+            for sta_obj in net_obj:
+                lon.append(sta_obj.longitude)
+                lat.append(sta_obj.latitude)
+                elv.append(sta_obj.elevation)
+                for cha_obj in sta_obj:
+                    if cha_obj.code in acceptable_channels:
+                        if cha_obj.code[-1] in cmpts:
+                            cha.append(cha_obj.code)
+
+        # check that there are channels
+        if not cha:
+            warnings.warn(f"Skipping {file_}: no channels found.")
+
+        # eliminate repeated channels
+        cha = list(set(cha))
+
+        # check that the channels correspond to different components
+        cha_cmp = []
+        for c in cha:
+            cha_cmp.append(c[-1])
+
+        if len(set(cha_cmp)) != len(cha_cmp):
+            warnings.warn(f"Skipping {file_}: multiple channels contain "
+                          f"information about the same component.")
+            continue
+
+        # check that there is information for all the requested components
+        if cha_cmp.sort() != cmpts.sort():
+            warnings.warn(f"Skipping {file_}: not all the requested components "
+                          f"are available")
+
+        # check that the station coordinates did not change over its deployment
+        if len(set(lon)) > 1 or len(set(lat)) > 1 or len(set(elv)) > 1:
+            warnings.warn(f"Skipping {file_}: contains different coordinates "
+                          f"for the same station.")
+            continue
+
+        code = f"{inv_net}.{inv_sta}"
+        stations[code] = {"net":inv_net,
+                          "sta":inv_sta,
+                          "lon":lon[0],
+                          "lat":lat[0],
+                          "elv":elv[0],
+                          "cha":cha,
+                         }
+    return stations
 
 
-def scan_waveforms(par):
+def scan_pairs(stations):
+    """
+    """
+    pairs = {}
+
+    stations_codes = list(stations.keys())
+    stations_codes.sort()
+    pairs_obj = itertools.combinations(stations_codes, 2)
+
+    for pair in pairs_obj:
+        s1 = pair[0]
+        s1lon = stations[s1]['lon']
+        s1lat = stations[s1]['lat']
+
+        s2 = pair[1]
+        s2lon = stations[s2]['lon']
+        s2lat = stations[s2]['lat']
+
+        dis, az, baz = gps2dist_azimuth(lat1=s1lat,
+                                        lon1=s1lon,
+                                        lat2=s2lat,
+                                        lon2=s2lon)
+
+        dis /= 1000.0  # convert from m to km
+
+        code = f"{s1}_{s2}"
+        pairs[code] = {"dis":dis,
+                       "az":az,
+                       "baz":baz,
+                       "ncorr":0
+                      }
+    return pairs
+
+
+def scan_waveforms(data_path, stations):
     """
     List relative path of all waveforms files inside data_path
     and their time span
@@ -54,57 +150,50 @@ def scan_waveforms(par):
     """
 
     waveforms_paths = []
-    start_dates = []
-    end_dates = []
+    stations_recording_start = []
+    stations_recording_end = []
 
-    networks = [x for x in os.listdir(par['data_path'])
-                if os.path.isdir(os.path.join(par['data_path'], x))]
+    stations_codes = list(stations.keys())
+    stations_codes.sort()
 
-    for net in networks:
-        if par['ignore_net'] and net in par['ignore_net']:
-            continue
+    for sta_code in stations_codes:
+        net, sta = sta_code.split(".")
 
-        net_path = os.path.join(par['data_path'], net)
+        sta_waveforms_files = os.listdir(os.path.join(data_path,net,sta))
+        sta_waveforms_files.sort()
 
-        stations = [x for x in os.listdir(net_path)
-                    if os.path.isdir(os.path.join(net_path, x))]
+        # keep only waveforms from desired channels
+        sta_waveforms_files = [x for x in sta_waveforms_files
+                               if x.split(".")[2] in stations[sta_code]["cha"]]
 
-        for sta in stations:
-            if par['ignore_sta'] and sta in par['ignore_sta']:
-                continue
+        # list waveforms paths
+        sta_waveforms_paths = [os.path.join(net,sta,x)
+                               for x in sta_waveforms_files]
+        sta_waveforms_paths.sort()
 
-            sta_path = os.path.join(net_path, sta)
+        waveforms_paths.extend(sta_waveforms_paths)
 
-            # list relative path
-            sta_waveforms_files = os.listdir(sta_path)
-            sta_waveforms_files.sort()
+        # get the first and last day where the station recorded data
+        first_waveform = sta_waveforms_files[0]
+        last_waveform = sta_waveforms_files[-1]
 
-            sta_waveforms_paths = ["{}/{}/{}".format(net, sta, x)
-                                   for x in sta_waveforms_files]
-            sta_waveforms_paths.sort()
+        start_day = first_waveform.split(".")[4].split("T")[0]
+        end_day = last_waveform.split(".")[5].split("T")[0]
 
-            waveforms_paths.extend(sta_waveforms_paths)
-
-            # determine time span
-            first_file = sta_waveforms_files[0].split(".")
-            last_file = sta_waveforms_files[-1].split(".")
-
-            start = first_file[4].split("T")[0]
-            end = last_file[5].split("T")[0]
-
-            start_dates.append(start)
-            end_dates.append(end)
+        stations_recording_start.append(start_day)
+        stations_recording_end.append(end_day)
 
     waveforms_paths.sort()
+    stations_recording_start.sort()
+    stations_recording_end.sort()
 
-    start_dates.sort()
-    end_dates.sort()
-
-    start = start_dates[0]
-    end = end_dates[-1]
+    # list the data span in a list of days
+    start = stations_recording_start[0]
+    end = stations_recording_end[-1]
 
     d1 = UTCDateTime(start).date
     d2 = UTCDateTime(end).date
+
     days = [str(d1 + datetime.timedelta(days=x))
             for x in range((d2-d1).days + 1)]
     data_span = np.array(days)
@@ -112,107 +201,37 @@ def scan_waveforms(par):
     return waveforms_paths, data_span
 
 
-def scan_stations(par):
-    """
-    Reads station metadata from ObsPy compatible files and returns
-    a PANDAS dataframe listing the stations and their information
+def list_waveforms_perday(waveforms_paths, data_span):
+    waveforms_paths_perday = {}
 
-    Thre must be one metadata file per station and each file must
-    contain information from one channel (component) only.
+    for day in data_span:
+        # determine file-name prefix for the day
+        day1 = day.replace("-", "")
+        day2 = UTCDateTime(day) + 86400
+        day2 = str(day2.date)
+        day2 = day2.replace("-", "")
+        day_prefix = f"{day1}T000000Z.{day2}T000000Z"
 
-    """
+        day_waveforms = []
 
-    code = []
-    cols = {'net': [],
-            'sta': [],
-            'cmp': [],
-            'loc': [],
-            'lat': [],
-            'lon': [],
-            'elv': []}
+        for i, wpath in enumerate(waveforms_paths):
+            fname = os.path.basename(wpath)
+            if day_prefix in fname:
+                day_waveforms.append(waveforms_paths[i])
 
-    metadata_files = os.listdir(par['metadata_path'])
+        day_waveforms.sort()
+        waveforms_paths_perday[day] = day_waveforms
 
-    for file_name in metadata_files:
-        inv = read_inventory(os.path.join(par['metadata_path'], file_name))
+    # verify that all waveforms are listed
+    counter = 0
+    for key in waveforms_paths_perday.keys():
+        counter += len(waveforms_paths_perday[key])
 
-        # list unique channels
-        channels = inv.get_contents()['channels']
-        channels = list(set(channels))
-
-        if len(channels) > 1:
-            print("Inventory contains more than one channel.")
-
-        net, sta, loc, cmp = channels[0].split(".")
-
-        if par['ignore_net'] and net in par['ignore_net']:
-            continue
-
-        if par['ignore_sta'] and sta in par['ignore_sta']:
-            continue
-
-        code.append("%s.%s" % (net, sta))
-        cols['net'].append(net)
-        cols['sta'].append(sta)
-        cols['cmp'].append(cmp)
-        cols['loc'].append(loc)
-        cols['lat'].append(inv[0][0].latitude)
-        cols['lon'].append(inv[0][0].longitude)
-        cols['elv'].append(inv[0][0].elevation)
-
-    stations = pd.DataFrame(cols)
-    stations.index = code
-    stations.sort_index(inplace=True)
-
-    return stations
-
-
-def scan_pairs(par, stations):
-    """
-    Defines station pairs given a PANDAS dataframe with the
-    stations information
-
-    Returns a PANDAS dataframe with station pairs information
-
-    """
-
-    code = []
-    cols = {'dis': [],
-            'az': [],
-            'baz': [],
-            'correlations': []}
-
-    pairs = itertools.combinations(stations.index, 2)
-
-    for pair in pairs:
-        s1 = pair[0]
-        s1lat = stations['lat'][s1]
-        s1lon = stations['lon'][s1]
-
-        s2 = pair[1]
-        s2lat = stations['lat'][s2]
-        s2lon = stations['lon'][s2]
-
-        d, a, b = gps2dist_azimuth(lat1=s1lat,
-                                   lon1=s1lon,
-                                   lat2=s2lat,
-                                   lon2=s2lon)
-        d /= 1000.0  # convert from m to km
-
-        net1, sta1 = s1.split('.')
-        net2, sta2 = s2.split('.')
-        c = "%s.%s_%s.%s" % (net1, sta1, net2, sta2)
-
-        code.append(c)
-        cols['dis'].append(d)
-        cols['az'].append(a)
-        cols['baz'].append(b)
-        cols['correlations'].append(0)
-
-    pairs = pd.DataFrame(cols)
-    pairs.index = code
-
-    return pairs
+    if counter == len(waveforms_paths):
+        return waveforms_paths_perday
+    else:
+        print("Error: not all waveforms were listed\n")
+        return
 
 
 def save_project(project, filename):
